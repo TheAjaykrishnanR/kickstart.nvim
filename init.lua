58eecc04e94668002e2707224493c365fb020386
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-09-30 02:12:02 +0530

    easy_align

--- init.lua
+++ init.lua
@@ -1225,6 +1225,10 @@ require('lazy').setup({
     -- this is equivalent to setup({}) function
   },
 
+  {
+    'junegunn/vim-easy-align',
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
