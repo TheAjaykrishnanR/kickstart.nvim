df19d1749fae3b65648f9915408c8ecb94d5e59d
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2026-08-17 00:33:10 +0530

    update

--- init.lua
+++ init.lua
@@ -528,7 +528,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -838,113 +838,113 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>de', '<cmd>:DapTerminate<CR>')
     end,
   },
-  { -- Debugger UI
-    'rcarriga/nvim-dap-ui',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-      'nvim-neotest/nvim-nio',
-    },
-    config = function()
-      local dap, dapui = require 'dap', require 'dapui'
+  --{ -- Debugger UI
+  --  'rcarriga/nvim-dap-ui',
+  --  dependencies = {
+  --    'mfussenegger/nvim-dap',
+  --    'nvim-neotest/nvim-nio',
+  --  },
+  --  config = function()
+  --    local dap, dapui = require 'dap', require 'dapui'
 
-      -- Setup dap-ui with default configuration
-      dapui.setup {
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-        mappings = {
-          -- Use a table to apply multiple mappings
-          expand = { '<CR>', '<2-LeftMouse>' },
-          open = 'o',
-          remove = 'd',
-          edit = 'e',
-          repl = 'r',
-          toggle = 't',
-        },
-        layouts = {
-          {
-            elements = {
-              -- Elements can be strings or table with id and size keys.
-              { id = 'scopes', size = 0.25 },
-              'breakpoints',
-              'stacks',
-              'watches',
-            },
-            size = 40, -- 40 columns
-            position = 'left',
-          },
-          {
-            elements = {
-              'repl',
-              'console',
-            },
-            size = 0.25, -- 25% of total lines
-            position = 'bottom',
-          },
-        },
-        controls = {
-          -- Requires Neovim nightly (or 0.8 when released)
-          -- Enabled by default for nightly builds
-          enabled = true,
-          -- Display controls in this element
-          element = 'repl',
-          icons = {
-            pause = '',
-            play = '',
-            step_into = '',
-            step_over = '',
-            step_out = '',
-            step_back = '',
-            run_last = '↻',
-            terminate = '□',
-          },
-        },
-        floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
-          border = 'single', -- Border style. Can be "single", "double" or "rounded"
-          mappings = {
-            close = { 'q', '<Esc>' },
-          },
-        },
-        windows = { indent = 1 },
-        render = {
-          max_type_length = nil, -- Can be integer or nil.
-          max_value_lines = 100, -- Can be integer or nil.
-        },
-      }
+  --    -- Setup dap-ui with default configuration
+  --    dapui.setup {
+  --      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+  --      mappings = {
+  --        -- Use a table to apply multiple mappings
+  --        expand = { '<CR>', '<2-LeftMouse>' },
+  --        open = 'o',
+  --        remove = 'd',
+  --        edit = 'e',
+  --        repl = 'r',
+  --        toggle = 't',
+  --      },
+  --      layouts = {
+  --        {
+  --          elements = {
+  --            -- Elements can be strings or table with id and size keys.
+  --            { id = 'scopes', size = 0.25 },
+  --            'breakpoints',
+  --            'stacks',
+  --            'watches',
+  --          },
+  --          size = 40, -- 40 columns
+  --          position = 'left',
+  --        },
+  --        {
+  --          elements = {
+  --            'repl',
+  --            'console',
+  --          },
+  --          size = 0.25, -- 25% of total lines
+  --          position = 'bottom',
+  --        },
+  --      },
+  --      controls = {
+  --        -- Requires Neovim nightly (or 0.8 when released)
+  --        -- Enabled by default for nightly builds
+  --        enabled = true,
+  --        -- Display controls in this element
+  --        element = 'repl',
+  --        icons = {
+  --          pause = '',
+  --          play = '',
+  --          step_into = '',
+  --          step_over = '',
+  --          step_out = '',
+  --          step_back = '',
+  --          run_last = '↻',
+  --          terminate = '□',
+  --        },
+  --      },
+  --      floating = {
+  --        max_height = nil, -- These can be integers or a float between 0 and 1.
+  --        max_width = nil, -- Floats will be treated as percentage of your screen.
+  --        border = 'single', -- Border style. Can be "single", "double" or "rounded"
+  --        mappings = {
+  --          close = { 'q', '<Esc>' },
+  --        },
+  --      },
+  --      windows = { indent = 1 },
+  --      render = {
+  --        max_type_length = nil, -- Can be integer or nil.
+  --        max_value_lines = 100, -- Can be integer or nil.
+  --      },
+  --    }
 
-      -- Auto-open/close UI when debugging starts/stops
-      dap.listeners.after.event_initialized['dapui_config'] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated['dapui_config'] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited['dapui_config'] = function()
-        dapui.close()
-      end
+  --    -- Auto-open/close UI when debugging starts/stops
+  --    dap.listeners.after.event_initialized['dapui_config'] = function()
+  --      dapui.open()
+  --    end
+  --    dap.listeners.before.event_terminated['dapui_config'] = function()
+  --      dapui.close()
+  --    end
+  --    dap.listeners.before.event_exited['dapui_config'] = function()
+  --      dapui.close()
+  --    end
 
-      -- Optional: Add keybindings for manual control
-      -- vim.keymap.set('n', '<leader>du', function()
-      -- dapui.toggle()
-      -- end, { desc = 'Toggle DAP UI' })
-      -- vim.keymap.set('n', '<leader>dr', function()
-      -- dapui.open { reset = true }
-      -- end, { desc = 'Reset DAP UI' })
-      -- vim.keymap.set('n', '<leader>dc', function()
-      -- dapui.close()
-      -- end, { desc = 'Close DAP UI' })
+  --    -- Optional: Add keybindings for manual control
+  --    -- vim.keymap.set('n', '<leader>du', function()
+  --    -- dapui.toggle()
+  --    -- end, { desc = 'Toggle DAP UI' })
+  --    -- vim.keymap.set('n', '<leader>dr', function()
+  --    -- dapui.open { reset = true }
+  --    -- end, { desc = 'Reset DAP UI' })
+  --    -- vim.keymap.set('n', '<leader>dc', function()
+  --    -- dapui.close()
+  --    -- end, { desc = 'Close DAP UI' })
 
-      -- Evaluate expressions
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>de', function()
-      -- dapui.eval()
-      -- end, { desc = 'Evaluate expression' })
-    end,
-  },
-  { -- Typescript lsp
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-  },
+  --    -- Evaluate expressions
+  --    -- vim.keymap.set({ 'n', 'v' }, '<leader>de', function()
+  --    -- dapui.eval()
+  --    -- end, { desc = 'Evaluate expression' })
+  --  end,
+  --},
+  --{ -- Typescript lsp
+  --  'pmizio/typescript-tools.nvim',
+  --  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --  opts = {},
+  --},
   { -- Powershell LSP
     'TheLeoP/powershell.nvim',
     ---@type powershell.user_config
